8cbc0dc6fe4ac98652525c5eab8258f18f684343
Cindy Royal <cindy@codeactually.com>
2015-11-12 14:29:44 -0600

    CSV Again

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
+import csv
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
+
+def get_csv():
+    csv_path = './static/la-riots-deaths.csv'
+    csv_file = open(csv_path, 'rb')
+    csv_obj = csv.DictReader(csv_file)
 
 @app.route("/")
 def index():
     template = 'index.html'
     return render_template(template)
-    
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
